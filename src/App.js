11395f3a5c6e8c7e7d96bdf2bae7dcb6b5c23ae7
thomasjfcaldwell <thomasjfcaldwell@gmail.com>
2020-07-30 14:45:05 -0700

    data in and displaying

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import DashBoard from './Dashboard';
-
+import axios from 'axios';
+import { Link, Route } from 'react-router-dom';
 
 const key = process.env.REACT_APP_MYAPI_KEY;
 
-
 class App extends Component {
+	constructor() {
+		super();
+		this.state = {
+			data: [],
+		};
+	}
+	componentDidMount() {
+		axios(
+			'https://www.potterapi.com/v1/characters/?key=$2a$10$jMMAc6AU4i8uxxNpsy9ImOMOEDXTf6rzLDcdWq2E9FAR0.TmcNA9G'
+		)
+			.then((json) => {
+				this.setState({ data: json.data });
+				console.log(this.state.data[19].name);
+			})
+			.catch(console.error);
+	}
 	render() {
 		return (
 			<div>
 				<Header />
-				<DashBoard />
+				<Route
+					path='/'
+					render={() => {
+						return <DashBoard data={this.state.data} />;
+					}}
+				/>
 			</div>
 		);
 	}
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
-import Header from "./Header"
+import Header from './Header';
+import './Dashboard.css';
 
 class DashBoard extends Component {
 	render() {
+		console.log(this.props.data);
 		return (
 			<div>
 				<Header />
-				<h1>Whats up dog?</h1>
+				<h1>Wizards</h1>
+				<ul>
+					{this.props.data.map((data, id) => {
+						return <li key={id}>{data.name}{" "}{data.role}</li>;
+					})}
+				</ul>
 			</div>
 		);
 	}
